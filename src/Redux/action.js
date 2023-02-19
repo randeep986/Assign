17444690419daeb8ff09e7c6fe0cbd99f20f3dad
Randeep Singh <randeepsingh986@gmail.com>
2023-02-20 02:09:55 +0530

    Adding categories

--- src/Redux/action.js
+++ src/Redux/action.js
@@ -1,12 +1,14 @@
 import * as types from  './actionType'
 import axios from 'axios'
 const getMoviesData=(dispatch)=>{
+    let baseUrl=process.env.REACT_APP_MY_ENVIRONMENT_BASE_URL
+    console.log(baseUrl)
     let key=process.env.REACT_APP_MY_ENVIRONMENT_API_KEY
     
     dispatch({
         type:types.GET_DATA_REQUEST
     })
-    return axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${ke}&language=en-US&page=1`).then((r)=>{
+    return axios.get(`${baseUrl}/movie/popular?api_key=${key}&language=en-US&page=1`).then((r)=>{
         console.log(r.data.results)
         dispatch({
             type:types.GET_DATA_SUCCESS
